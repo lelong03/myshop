3a5d8dc35986946c374213dd160301ef8c5e02a7
Long Le <long.le@ved.com.vn>
2018-04-20 18:04:27 +0700

    integrate with celery

--- orders/tasks.py
+++ orders/tasks.py
@@ -1,4 +1,6 @@
 from celery import task
+from celery.app import shared_task
+from celery.utils.log import get_task_logger
 from django.core.mail import send_mail
 from .models import Order
 
@@ -11,11 +13,23 @@ def order_created(order_id):
     """
     order = Order.objects.get(id=order_id)
     order.paid = True
+    import time
+    time.sleep(60)
     order.save()
     
-    subject = 'Order nr. {}'.format(order.id)
-    message = 'Dear {},\n\nYou have successfully placed an order.\
-                  Your order id is {}.'.format(order.first_name, order.id)
-    mail_sent = send_mail(subject, message, 'admin@myshop.com', [order.email])
-    return mail_sent
+    # subject = 'Order nr. {}'.format(order.id)
+    # message = 'Dear {},\n\nYou have successfully placed an order.\
+    #               Your order id is {}.'.format(order.first_name, order.id)
+    # mail_sent = send_mail(subject, message, 'admin@myshop.com', [order.email])
+    # return mail_sent
 
+    return True
+
+
+logger = get_task_logger(__name__)
+
+@shared_task
+def debug_task(msg):
+    import time
+    time.sleep(10)
+    logger.info("debug|msg=%s", msg)
